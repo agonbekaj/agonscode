a7df193bd9611c7f568cc33bd67b9bbd920230fc
Agon Bekaj <agonbekaj97@gmail.com>
2023-01-09 00:18:37 +0100

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <div className="footer__container container">
+            <h1 className="footer__title">Bekaj</h1>
+
+            <ul className="footer__list">
+                <li>
+                    <a href="#about" className="footer__link">About</a>
+                </li>
+
+                <li>
+                    <a href="#porfolio" className="footer__link">Projects</a>
+                </li>
+
+                <li>
+                    <a href="#testimonials" className="footer__link">Testimonials</a>
+                </li>
+            </ul>
+
+            <div className="footer__social">
+            <a href="https://www.facebook.com/" className="footer__social-link" target="_blank">
+            <i class="bx bxl-facebook"></i>
+        </a>
+
+        <a href="https://www.instagram.com/" className="footer__social-link" target="_blank">
+            <i class="bx bxl-instagram"></i>
+        </a>
+
+        <a href="https://www.twitter.com/" className="footer__social-link" target="_blank">
+            <i class="bx bxl-twitter"></i>
+        </a>
+            </div>
+
+            <span className="footer__copy">
+                &#169; agonscode. All rights reserved</span>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/agonbekaj" className="home__social-icon" target="_blank">
+            <i class="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://www.github.com/agonbekaj" className="home__social-icon" target="_blank">
+            <i class="uil uil-github-alt"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default social
